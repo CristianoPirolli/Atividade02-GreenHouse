d7151be55e116c92574190e143764aa775ed26c4
Cristiano Pirolli <cristianopirolli@hotmail.com>
2025-08-18 20:29:04 -0300

    Adicionar arquivos de projeto.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using GreenHouse.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace GreenHouse.Data
+{
+    public class AppDbContext : DbContext
+    {
+        //  Essa propriedade representa a TABELA no banco
+        public DbSet<Plant> Plants { get; set; }
+
+        //  Construtor que recebe as opções (como connection string)
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        //  Configurações adicionais (se necessário)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Define chave primária personalizada e tipo de coluna
+            modelBuilder.Entity<Plant>()
+                .HasKey(p => p.PlantName)
+                .HasName("PK_PLANTS");
+
+            modelBuilder.Entity<Plant>()
+                .Property(p => p.SensorEvent)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                .ValueGeneratedOnAddOrUpdate();
+        }
+    }
+}
---
+++ Models/Plant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GreenHouse.Models
+{
+    [Table("PLANTS")] // Mapeia para a tabela do banco
+    public class Plant
+    {
+        [Key]
+        [Column("PLANT_NAME", TypeName = "char(30)")]
+        public string PlantName { get; set; }
+
+        [Column("SENSOR_VALUE")]
+        public float SensorValue { get; set; }
+
+        [Column("SENSOR_EVENT")]
+        public DateTime SensorEvent { get; set; } = DateTime.Now;
+    }
+}
